cf5efd3dac750694031da0316412cf7c2cc07873
alemg <alem.gusinac@radboudumc.nl>
2025-07-02 17:55:18 +0200

    changed pandas module to polars, updated container

--- bin/python/merge_multiqc_stats.py
+++ bin/python/merge_multiqc_stats.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-import pandas as pd
+import polars as pl
 
 #----------------------------------------------------------------------------#
 # Parse command line arguments
@@ -11,34 +11,46 @@ parser.add_argument('--i-decon', dest='decon_reads', type=str, help='in filename
 parser.add_argument('-o','--outfile', dest='outfile', type=str, help='out filename', default='merged_read_stats.tsv')
 options = vars(parser.parse_args())
 
-print(options)
-
 ## Main code
-raw_df = pd.read_csv(options['raw_reads'],
-                     sep = "\t",
-                     usecols = ["Sample", "Total Sequences"])
-raw_df.columns = ["Sample", "Input Reads"]
+# Read raw reads
+raw_df = pl.read_csv(
+    options['raw_reads'],
+    separator="\t",
+    columns=["Sample", "Total Sequences"]
+).rename({"Total Sequences": "Input Reads"})
 
+# Optionally add trimmed reads
 if options['trim_reads']:
-    trim_df = pd.read_csv(options['trim_reads'],
-                          sep = "\t",
-                          usecols = ["Sample", "Total Sequences"])
+    trim_df = pl.read_csv(
+        options['trim_reads'],
+        separator="\t",
+        columns=["Sample", "Total Sequences"]
+    )
+    # Join to get Input Reads for percentage calculation
+    trim_df = trim_df.join(raw_df, on="Sample")
+    trim_df = trim_df.with_columns(
+        (pl.col("Total Sequences") / pl.col("Input Reads") * 100).round(2).alias("Trimmed Reads %")
+    ).rename({"Total Sequences": "Trimmed Reads"})
+    trim_df = trim_df.select(["Sample", "Trimmed Reads", "Trimmed Reads %"])
+    raw_df = raw_df.join(trim_df, on="Sample", how="left")
 
-    trim_df["Trimmed Reads %"] = round(trim_df["Total Sequences"] / raw_df["Input Reads"] * 100, 2)
-    trim_df.columns = ["Sample", "Trimmed Reads", "Trimmed Reads %"]
-    raw_df = raw_df.merge(trim_df, on='Sample')
-
+# Optionally add decontaminated reads
 if options['decon_reads']:
-    decon_df = pd.read_csv(options['decon_reads'],
-                           sep = "\t",
-                           usecols = ["Sample", "Total Sequences"])
-
-    decon_df["Clean Reads %"] = round(decon_df["Total Sequences"] / raw_df["Input Reads"] * 100, 2)
-    decon_df.columns = ["Sample", "Clean Reads", "Clean Reads %"]
-    raw_df = raw_df.merge(decon_df, on='Sample')
+    decon_df = pl.read_csv(
+        options['decon_reads'],
+        separator="\t",
+        columns=["Sample", "Total Sequences"]
+    )
+    # Join to get Input Reads for percentage calculation
+    decon_df = decon_df.join(raw_df, on="Sample")
+    decon_df = decon_df.with_columns(
+        (pl.col("Total Sequences") / pl.col("Input Reads") * 100).round(2).alias("Clean Reads %")
+    ).rename({"Total Sequences": "Clean Reads"})
+    decon_df = decon_df.select(["Sample", "Clean Reads", "Clean Reads %"])
+    raw_df = raw_df.join(decon_df, on="Sample", how="left")
 
 # outfile
-raw_df.to_csv(
+raw_df.write_csv(
     options['outfile'],
-    sep = "\t",
+    separator = "\t",
     )
